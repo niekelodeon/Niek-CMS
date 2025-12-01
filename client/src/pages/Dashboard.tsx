100be2e1339b48b5d960014e603ce5f4cbc18834
niekelodeon <niekamperse2@proton.me>
2025-12-01 15:17:19 +0100

    Made changes on the edit/dashboard page according to Figma design

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ import FolderTree from '../components/FolderTree'
 export default function Dashboard() {
     return (
         <div id="container-page" className="min-height-screen">
-            <div id="container-layout" className="grid grid-cols-2 grid-rows-[1fr_auto] px-30 pt-15">
+            <div id="container-layout" className="grid grid-cols-2 grid-rows-[1fr_auto] gap-[4rem] pl-[10%] pr-[5%] pt-15 pb-15">
                 <div id="container-folderTree" className="height-screen">
                     <FolderTree />
                 </div>
@@ -16,3 +16,5 @@ export default function Dashboard() {
         </div>
     )
 }
+
+// 1250px wide is the minimum
