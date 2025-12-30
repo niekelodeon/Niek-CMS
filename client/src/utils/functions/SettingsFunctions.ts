22d8cbabe09276586d3e78c85616b3e5c063969a
niekelodeon <niekamperse2@proton.me>
2025-12-30 11:22:20 +0100

    SettingsFunction.ts cleanup

--- client/src/utils/functions/SettingsFunctions.ts
+++ client/src/utils/functions/SettingsFunctions.ts
@@ -1,12 +1,11 @@
 import { getDefaultStore } from 'jotai'
 
-import { currentNodeAtom, resultMessageAtom } from '../atoms'
+import { saveConnectionResultAtom, saveConnectionMessageAtom } from '../atoms'
 
 import { settingsAPI } from '../API'
 
 import type { Connection, GetConnectionResponse, SaveConnectionResponse } from '../interfaces'
 
-// use different casing in class names
 export class settingsFunctions {
     public static async getConnection(): Promise<Connection> {
         try {
@@ -14,20 +13,20 @@ export class settingsFunctions {
 
             return getConnectionObject.data
         } catch (err) {
-            // set error message here to a atom
             return err
         }
     }
 
-    // Unfinished, type of getConnectionResponse for now, should be "saveConnectionResponse" but don't know what type response it will give yet. result & message probably.
     public static async saveConnection(connection: Connection): Promise<SaveConnectionResponse> {
+        const store = getDefaultStore()
+
         try {
             const saveConnectionObject: SaveConnectionResponse = await settingsAPI.saveConnection(connection)
 
             return saveConnectionObject
         } catch (err) {
-            // set error message here to a atom
-            return err
+            store.set(saveConnectionResultAtom, Boolean(false))
+            store.set(saveConnectionMessageAtom, String(err))
         }
     }
 }
