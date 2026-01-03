21d6266dfa93ba044d6a4f8e7a4eaef229b525c0
niekelodeon <niekamperse2@proton.me>
2026-01-03 11:57:57 +0100

    RemoteStructure.tsx update

--- client/src/components/RemoteStructure.tsx
+++ client/src/components/RemoteStructure.tsx
@@ -11,10 +11,30 @@ export default function RemoteStructure() {
     const [checkConnectionResult, setCheckConnectionResult] = useAtom(checkConnectionResultAtom)
     const [checkConnectionMessage, setCheckConnectionMessage] = useAtom(checkConnectionMessageAtom)
 
-    async function checkConnection() {
+    async function checkConnection(e: any) {
+        e.preventDefault()
+
         const checkConnectionObject = await settingsFunctions.checkConnection()
 
         setCheckConnectionResult(checkConnectionObject.result)
         setCheckConnectionMessage(checkConnectionObject.message)
     }
+
+    // it's probably better to click on fetchRemoteStructure then users can see if the connection is correct or not without needing a extra button.
+
+    return (
+        <div id="container-button" className="flex w-[21.25rem] flex-col gap-[1rem] transition-all duration-[900ms]">
+            <form onSubmit={checkConnection} id="container-inputs" className="flex flex-col items-center gap-[1.875rem]">
+                <button type="submit" className="flex-start flex w-[21.25rem] cursor-pointer rounded-md bg-[#7F7EFF] px-[2.5rem] py-3 font-medium transition-[900ms] hover:bg-[#5D5CC9]">
+                    Test Connection!
+                </button>
+
+                <div id="message" className={`text-sm ${checkConnectionResult ? 'text-[#4ade80]' : 'text-[#ff8082]'}`}>
+                    {checkConnectionMessage}
+                </div>
+            </form>
+
+            <div className="container-selectRemoteStructure"></div>
+        </div>
+    )
 }
--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,11 +1,16 @@
 import Connection from '../components/Connection'
+import RemoteStructure from '../components/RemoteStructure'
 
 export default function Settings() {
     return (
         <div id="container-page" className="max-h-screen overflow-hidden">
             <div id="container-layout" className="h-full">
-                <div className="container-settings">
+                <div className="container-connection">
                     <Connection />
+                </div>
+
+                <div id="container-remoteStructure">
+                    <RemoteStructure />
                 </div>
             </div>
         </div>
